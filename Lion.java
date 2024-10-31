a9e31ba8129553b2ab818521b192ed2b9b813e8e
Tyler <157324479+LispSCPSL@users.noreply.github.com>
2024-10-31 12:24:02 -0700

    Add files via upload

---
+++ Lion.java
@@ -0,0 +1,20 @@
+package tyler.zoo.com;
+
+public class Lion extends tyler.zoo.com.AnimalOct3 {
+    static int numOfLions = 0; // Created static int that keeps track of the num of animals created
+
+
+    public Lion() {
+        super();
+        numOfLions++;
+    }
+
+
+    // Create a constructor that will have all the fields I want.
+    public Lion(String sex, int age, int weight, String animalName, String animalID,
+                     String animalBirthdate, String animalColor, String animalOrigin){
+        super(sex, age, weight, animalName, animalID, animalBirthdate, animalColor, animalOrigin);
+        numOfLions++;
+    }
+
+}
---
+++ Tiger.java
@@ -0,0 +1,20 @@
+package tyler.zoo.com;
+
+public class Tiger extends tyler.zoo.com.AnimalOct3 {
+    static int numOfTigers = 0; // Created static int that keeps track of the num of animals created
+
+
+    public Tiger() {
+        super();
+        numOfTigers++;
+    }
+
+
+    // Create a constructor that will have all the fields I want.
+    public Tiger(String sex, int age, int weight, String animalName, String animalID,
+                String animalBirthdate, String animalColor, String animalOrigin){
+        super(sex, age, weight, animalName, animalID, animalBirthdate, animalColor, animalOrigin);
+        numOfTigers++;
+    }
+
+}
